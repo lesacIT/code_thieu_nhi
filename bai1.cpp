d48e95d745c091cb70340bd8c7a7a4d359307174
LeSac <107132448+lesacIT@users.noreply.github.com>
2023-06-14 12:48:51 +0700

    Update bai1.cpp

--- bai1.cpp
+++ bai1.cpp
@@ -8,8 +8,8 @@ int main(){
 	
 	printf("nhap dien tich S: ");
 	scanf("%lf", &S);
-	printf("The tich V= %g\n"),
-	(4*M_Pi *pow(sqrt(S/(4*M_PI)),3))/3);
+	printf("The tich V= %g\n",
+	 (4* M_PI * pow(sqrt( S / (4* M_PI)),3)) /3 );
 	
 	return 0;
 }
