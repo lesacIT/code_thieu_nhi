c1efbe3d3fe695afc0e1fdd538e04c61ba1a3cfd
LeSac <107132448+lesacIT@users.noreply.github.com>
2023-06-14 12:45:06 +0700

    bai 1

---
+++ bai1.cpp
@@ -0,0 +1,15 @@
+// nhap vao dien tich s cua mat cau .tinh the tich v cua hinh cau nay
+
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	double S;
+	
+	printf("nhap dien tich S: ");
+	scanf("%lf", &S);
+	printf("The tich V= %g\n"),
+	(4*M_Pi *pow(sqrt(S/(4*M_PI)),3))/3);
+	
+	return 0;
+}
