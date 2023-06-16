29e6b8bcc51853723da64644004fc98a342cde9a
LeSac <107132448+lesacIT@users.noreply.github.com>
2023-06-16 22:31:11 +0700

    bai 12

---
+++ bai12.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+	float a1,b1,c1,a2,b2,c2,dx,dy,d;
+	
+	printf(" nhap a1,b1,c1: ");
+	scanf("%f%f%f",&a1,&b1,&c1);
+	printf(" nhap a2,b2,c2: ");
+	scanf("%f%f%f",&a2,&b2,&c2);
+	
+	d=(a1 * b2-a2*b1);
+	dx=(c1*b2-c2*b1);
+	dy=(a1*c2-a2*c1);
+	
+	if(!d) printf((!dx && !dy) ?"Vo dinh\n":"Vo nghiem\n");
+	else printf("x=%g\ny=%g\n",dx/d,dy/d);
+	return 0;
+}
