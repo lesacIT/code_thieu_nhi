913a40f430b644816c258fb4bddf52762572f261
LeSac <107132448+lesacIT@users.noreply.github.com>
2023-06-19 20:15:56 +0700

    bai 30

---
+++ bai30.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+	double rate,balance;
+	unsigned year,period,n;
+	
+	
+	do{
+		printf("nhap lai suat ,tien von,thoi han:");
+		n=scanf("%lf,%lf,%u",&rate,&balance,&period);
+		
+		while(getchar()!='\n'){
+		}
+		if(n!=3|| n==EOF)
+			printf("nhap thieu haoc nhap loi!\n");
+			
+	}while(n!=3|| n==EOF);
+	
+	printf("Lai suat:%g%%\n",rate *100);
+	printf("Von ban dau: %g\n",balance);
+	printf("Thoi han: %u nam\n",period);
+	printf("%3s%10s\n","Nam","Von");
+	for(year=1;year<=period;++year){
+		balance *=1+rate;
+		printf("%3u%10g\n",year,balance);
+	}
+	return 0;
+}
