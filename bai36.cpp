11e5229544ddeeb8e4bee00c227c8af23cff438a
LeSac <107132448+lesacIT@users.noreply.github.com>
2023-06-23 20:31:02 +0700

    bai 36

---
+++ bai36.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+	unsigned n,k,i,count;
+	
+	printf("Nhap n: ");
+	scanf("%u",&n);
+	
+	k=2;
+	count=0;
+	while(count<n){
+		for(i=2;i*i<=k;++i)
+			if(k%i==0)break;
+		if(i*i>k){
+			printf("%u ",k);
+			count++;
+		}
+		k++;
+	}
+	putchar('\n');
+	return 0;
+}
